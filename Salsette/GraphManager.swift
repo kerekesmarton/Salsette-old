7a78be66ea65250c28b3a29f7529051d50bb5a5c
Marton Kerekes <marton.kerekes@moonpig.com>
2017-10-17 09:52:20 +0100

    introduced authenticated user

--- Salsette/GraphManager.swift
+++ Salsette/GraphManager.swift
@@ -10,11 +10,13 @@ import Foundation
 import Apollo
 
 class GraphManager {
-    private  init() {}
+    private init() {}
     static let shared = GraphManager()
+    private let path = "https://eu-west-1.api.scaphold.io/graphql/dance"
     lazy var client: ApolloClient = {
-        return ApolloClient(url: URL(string: "https://eu-west-1.api.scaphold.io/graphql/dance")!)
+        return ApolloClient(url: URL(string: path)!)
     }()
+    var loggedInClient: ApolloClient?
     var isLoggedIn: Bool {
         get {
             return (token != nil) ? true : false
@@ -36,10 +38,12 @@ class GraphManager {
         operation = client.perform(mutation: Auth0LoginMutation(token: input), resultHandler: { (result, error) in
             if let serverError = result?.errors {
                 closure(false, self.error(from: serverError))
-            } else if let auth = result?.data?.loginUserWithAuth0,
-                let newUser = auth.user {
+            } else if let auth = result?.data?.loginUserWithAuth0, let newUser = auth.user, let url = URL(string: self.path) {
                 self.token = token
                 self.user = newUser
+                let configuration = URLSessionConfiguration.default
+                configuration.httpAdditionalHeaders = ["Authorization": "Bearer \(token)"]
+                self.loggedInClient = ApolloClient(networkTransport: HTTPNetworkTransport(url: url, configuration: configuration))
                 closure(true, error)
             }
             closure(false, error)
@@ -52,6 +56,10 @@ class GraphManager {
     }
     typealias CretedEvent = CreateEventMutation.Data.CreateEvent.ChangedEvent
     func createEvent(model: CreateEventModel, closure: @escaping (CretedEvent?, Error?)->Void) {
+        guard let client = loggedInClient else {
+            closure(nil, error(with: "Please log in"))
+            return
+        }
         let input = CreateEventInput(fbId: model.fbID, type: model.type, clientMutationId: nil)
         operation = client.perform(mutation: CreateEventMutation(event: input), resultHandler: { (result, error) in
             if let serverError = result?.errors {
@@ -64,6 +72,10 @@ class GraphManager {
     
     typealias EventSearchResult = FetchEventQuery.Data.Viewer.AllEvent.Edge.Node
     func searchEvent(fbID: String, closure: @escaping (EventSearchResult?, Error?)->Void) {
+        guard let client = loggedInClient else {
+            closure(nil, error(with: "Please log in"))
+            return
+        }
         let arg = EventFbIdWhereArgs(eq: fbID)
         let query = FetchEventQuery(where: EventWhereArgs(fbId:arg))
         operation = client.fetch(query: query, cachePolicy: .returnCacheDataElseFetch, queue: DispatchQueue.main, resultHandler: { (result, error) in
@@ -79,6 +91,10 @@ class GraphManager {
     
     typealias AllEventsSearchResult = FetchAllEventsQuery.Data.Viewer.AllEvent.Edge.Node
     func serchAllEvents(closure: @escaping ([AllEventsSearchResult]?, Error?)->Void) {
+        guard let client = loggedInClient else {
+            closure(nil, error(with: "Please log in"))
+            return
+        }
         let query = FetchAllEventsQuery()
         operation = client.fetch(query: query, cachePolicy: .returnCacheDataElseFetch, queue: DispatchQueue.main, resultHandler: { (result, error) in
             if let serverError = result?.errors {
@@ -99,6 +115,10 @@ class GraphManager {
     }
     typealias CretedWorkshop = CreateWorkshopMutation.Data.CreateWorkshop.ChangedWorkshop
     func createWorkshop(model: CreateWorkshopDataModel, closure: @escaping (CretedWorkshop?, Error?)->Void) {
+        guard let client = loggedInClient else {
+            closure(nil, error(with: "Please log in"))
+            return
+        }
         let input = CreateWorkshopInput(room: model.room, startTime: model.startTime, artist: model.artist, name: model.name, event: nil, eventId: model.eventID, clientMutationId: nil)
         operation = client.perform(mutation: CreateWorkshopMutation(workshop: input), resultHandler: { (result, error) in
             if let serverError = result?.errors {
@@ -109,8 +129,12 @@ class GraphManager {
             closure(nil, nil)
         })
     }
+
+    private func error(with message: String) -> Error {
+        return NSError(domain: "Client", code: 0, userInfo: [NSLocalizedDescriptionKey:message])
+    }
     
-    func error(from graphQLErrors: [GraphQLError]) -> Error {        
+    private func error(from graphQLErrors: [GraphQLError]) -> Error {
         guard let error = graphQLErrors.first else {
             return NSError(domain: "Apollo", code: 0, userInfo: [NSLocalizedDescriptionKey:"Unknown error"])
         }
